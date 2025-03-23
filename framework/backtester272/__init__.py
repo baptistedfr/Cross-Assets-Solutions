93ab8ebe2c2d9cd1a96fc2b235dcef5318d201ce
Thiboss <thibault.charbonnier@edu.devinci.fr>
2025-03-23 23:02:58 +0100

    Database macro

--- framework/backtester272/__init__.py
+++ framework/backtester272/__init__.py
@@ -3,3 +3,4 @@ from backtester272.Universe import Universe
 from backtester272.StrategyBank import *
 from backtester272.TacticalBank import *
 from backtester272.Result import Result
+from backtester272.DataMacro import DataMacro
